e24dc0dbdf956ff1d6a540dcb1677f5df578d28c
fatima493 <fatima.hajiyeva@ug.bilkent.edu.tr>
2025-02-05 19:37:08 +0300

    Create ArrayOperations.java
    Created the main base of the file and showed all the methods we need

---
+++ ArrayOperations.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+// 1.Generate an array of random integers in the range [0, 100]
+public class ArrayOperations {
+
+ 
+    }
+
+    // 2. Find the minimum value in the array
+    public static int findMin(int[] array) {
+
+    }
+
+    // 3. Find the maximum value in the array
+    public static int findMax(int[] array) {
+
+    }
+
+    // 4. Calculate and display the average, and differences from the average
+    public static void calculateAverageAndDifferences(int[] array) {
+
+    }
+
+    // 5. Calculate and display the sum of elements at odd and even indexes
+    public static void calculateSumOfIndexes(int[] array) {
+
+      
+    }
+
+    // Display the menu and get the user input
+    public static void displayMenuAndProcessChoice(int[] array) {
+       
+    }
+
+    public static void main(String[] args) {
+
+}
